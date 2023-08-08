0205b19886e85e94b0fd57b943847c0fdca953ab
ricklyon <rlyon14@gmail.com>
2023-08-07 21:57:35 -0600

    rename

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-from setuptools import setup, find_packages
-import os
+# setup.py
 
+from setuptools import setup
 
-setup(
-    name='structures',
-    description='dev package',
-    author='rlyon14',
-    version='0.1.1',
-    url="https://github.com/rlyon14/structures",
-    packages=['structures',],
-    install_requires=(
-        'numpy',
-        'pyserial',
-    ),
-)
+setup()
