8dc6f427973fb8a0898ea12f54b5fcdf7bca7f47
ricklyon <ricklyon@github.com>
2026-07-19 10:58:39 -0600

    add 2D interp test

--- tests/test_ldarray.py
+++ tests/test_ldarray.py
@@ -147,6 +147,26 @@ class TestLdArray(unittest.TestCase):
         np.testing.assert_array_almost_equal(ld_int.sel(a="exp(t)"), np.exp(1j * t_int), decimal=2)
         np.testing.assert_array_almost_equal(ld_int.sel(a="exp(0.5t)"), np.exp(0.5 * 1j * t_int), decimal=2)
 
+    def test_interpolation_2d(self):
+        coords = dict(a=[1, 2], b=['data1', 'data2', 'data3'])
+        ld = ldarray([[10, 8, 6], [0, 2, 4]], coords=coords)
+
+        a_int = ldarray(np.ones((2, 2)), coords=dict(x=[0, 1], y=[0, 1]))
+        data = ld.interpolate(a = a_int)
+
+        # interpolated shape is (3, 2, 2), 3 is the "b" column that is moved to the front, ahead
+        # of the 2x2 meshgrid. Each 2x2 array is the different values of b at a=1.
+        ref = np.array([
+            [[10, 10], [10, 10]],
+            [[ 8,  8], [ 8,  8]],
+            [[ 6,  6], [ 6,  6]]]
+        )
+        np.testing.assert_array_equal(data, ref)
+
+        # check coordinates
+        np.testing.assert_array_equal(data.coords["b"], coords["b"])
+        np.testing.assert_array_equal(data.coords["x"], [0, 1])
+        np.testing.assert_array_equal(data.coords["y"], [0, 1])
 
 
     def test_save(self):
